60a1a3f93628b167b0dd8b03b5f4539f7610ee58
Lucas Knudsen <laksteelhouse@gmail.com>
2021-03-03 23:30:52 +0100

    Adds model specs

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  describe 'validates existence of expected columns' do
+    it { is_expected.to have_db_column :title}
+    it { is_expected.to have_db_column :body}
+    it { is_expected.to have_db_column :teaser}
+    it { is_expected.to have_db_column :article_type}
+    it { is_expected.to have_db_column :created_at}
+  end
+  
   describe 'validates attributes' do
     it { should validate_presence_of(:article_type)}
     it { should validate_presence_of(:title)}
     it { should validate_presence_of(:teaser)}
     it { should validate_presence_of(:body)}
+  end
+
+  describe 'validates enum values of article_type attribute' do
+    it { should define_enum_for(:article_type).with_values([:experience, :story])}
   end
 end
