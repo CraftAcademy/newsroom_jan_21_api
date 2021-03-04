f84526e7036e5203aa7f515382bf1ca14b2071d6
FoxyTony <mirantony@icloud.com>
2021-03-04 10:54:09 +0100

    add unit spec

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+RSpec.describe Article, type: :model do
+  describe 'validates attributes' do
+    it { should validate_presence_of(:article_type)}
+    it { should validate_presence_of(:title)}
+    it { should validate_presence_of(:teaser)}
+    it { should validate_presence_of(:body)}
+  end
+
+  describe 'validate enum attribute' do
+    it { should define_enum_for(:article_type).with_values([:experience, :story]) }
+  end
+end
