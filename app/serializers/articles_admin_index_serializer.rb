2f3775f7dacf0131a0fdabfea84646a4f2193e9f
Lucas Knudsen <laksteelhouse@gmail.com>
2021-03-10 11:23:29 +0100

    Remakes the old admin index to provide all article data, so that it can be passed down to the edit modal on the client

--- app/serializers/articles_admin_index_serializer.rb
+++ app/serializers/articles_admin_index_serializer.rb
@@ -1,14 +1,21 @@
 class ArticlesAdminIndexSerializer < ActiveModel::Serializer
-  attributes :id, :title, :date
-  belongs_to :author, class_name: "Admin"
+  attributes :id, :title, :teaser, :body, :article_type, :category, :location, :image, :date
+  belongs_to :author, class_name: 'Admin'
 
   def date
     object.updated_at.strftime('%F')
   end
+
   def author
-   {
-     id: object.admin.id,
-    name: object.admin.name
-  }
+    {
+      id: object.admin.id,
+      name: object.admin.name
+    }
+  end
+
+  def image
+    return nil unless object.image.attached?
+
+    object.image_url
   end
 end
