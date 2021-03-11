5f6113b75ff3a3301fe6d55c9db20f5b55aaabb6
Lucas Knudsen <laksteelhouse@gmail.com>
2021-03-11 15:38:38 +0100

    Configures User model and tests validation functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'Admin', at: 'api/admin_auth'
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'api/auth'
 
   namespace :api do
     resources :articles, only: %i[index show]
